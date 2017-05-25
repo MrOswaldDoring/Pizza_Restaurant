178d5b25471de78a20ec512eb60bdb727f07acb5
MrOswaldDoring <oswald.doring98@hotmail.com>
2017-05-25 17:09:04 +1000

    Small comment change

--- src/asgn2Tests/LogHandlerPizzaTests.java
+++ src/asgn2Tests/LogHandlerPizzaTests.java
@@ -3,7 +3,7 @@ package asgn2Tests;
 
 /** A class that tests the methods relating to the creation of Pizza objects in the asgn2Restaurant.LogHander class.
 * 
-* @author Person B
+* @author Oswald Doring n9451269
 * 
 */
 public class LogHandlerPizzaTests {
