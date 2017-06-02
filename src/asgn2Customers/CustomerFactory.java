3ca522ad7de843e1d3923d01bcf6df0ae00a6273
MrOswaldDoring <oswald.doring98@hotmail.com>
2017-06-02 15:34:50 +1000

    minor changes due to errors

--- src/asgn2Customers/CustomerFactory.java
+++ src/asgn2Customers/CustomerFactory.java
@@ -8,7 +8,7 @@ import asgn2Exceptions.CustomerException;
  * The classes are instantiated from one of the three valid customer codes outlined in
  * Section 5.3 of the Assignment Specification. Any other code will throw a CustomerException.   
  *     
- * @author Person B
+ * @author Oswald Doring n9451269
  *
  */
 
