10057f4a3b17f227be2e046da865f725aa418076
MrOswaldDoring <oswald.doring98@hotmail.com>
2017-06-02 15:41:32 +1000

    minor changes due to errors

--- src/asgn2Tests/RestaurantPizzaTests.java
+++ src/asgn2Tests/RestaurantPizzaTests.java
@@ -2,6 +2,8 @@ package asgn2Tests;
 
 import org.junit.*;
 
+import asgn2Exceptions.CustomerException;
+import asgn2Exceptions.LogHandlerException;
 import asgn2Exceptions.PizzaException;
 import asgn2Pizzas.*;
 
@@ -18,30 +20,67 @@ import java.time.LocalTime;
  * A class that tests the methods relating to the handling of Pizza objects in the asgn2Restaurant.PizzaRestaurant class as well as
  * processLog and resetDetails.
  * 
- * @author Person B
+ * @author Oswald Doring n9451269
  *
  */
 public class RestaurantPizzaTests {
-	// TO DO
-	MeatLoversPizza meatLovers;
-	VegetarianPizza vegetarian;
-	MargheritaPizza margherita;
+	
+	String logOne = "logs\\20170101.txt";
+	String logTwo = "logs\\20170102.txt";
+	String logThree = "logs\\20170103.txt";
+	PizzaRestaurant pr;
+	MargheritaPizza testPizza;
 	
 	@Before
-	public void initalisePizzas() throws PizzaException{
-		PizzaRestaurant pr = new PizzaRestaurant();
-		LocalTime orderTime = LocalTime.of(20, 00);
-		LocalTime deliveryTime = LocalTime.of(20, 45);
-		int quantity = 1;
-		meatLovers = new MeatLoversPizza(quantity, orderTime, deliveryTime);
-		vegetarian = new VegetarianPizza(quantity, orderTime, deliveryTime);
-		margherita = new MargheritaPizza(quantity, orderTime, deliveryTime);
-		pr.
-		
+	public void setUpStuff() throws PizzaException{
+		pr = new PizzaRestaurant();
+		testPizza = new MargheritaPizza(1, LocalTime.of(20, 00), LocalTime.of(20, 25));
 	}
 	
-	@Test(Expected = PizzaException.class)
+	@Test
+	public void processLogOne() throws CustomerException, PizzaException, LogHandlerException{
+		pr.processLog(logOne);
+	}
+	
+	@Test
+	public void processLogTwo() throws CustomerException, PizzaException, LogHandlerException{
+		pr.processLog(logTwo);
+	}
+	
+	@Test
+	public void processLogThree() throws CustomerException, PizzaException, LogHandlerException{
+		pr.processLog(logThree);
+	}
+	
+	
+	@Test(expected = PizzaException.class)
 	public void pizzaIndexInvalid() throws PizzaException{
-		PizzaRestaurant.getPizzaByIndex(5);
+		pr.getPizzaByIndex(5);
+	}
+	
+	@Test
+	public void pizzaByIndex() throws PizzaException, CustomerException, LogHandlerException{
+		pr.processLog(logOne);
+		assertEquals(testPizza, pr.getPizzaByIndex(1));
+	}
+	
+	@Test
+	public void customerSize() throws PizzaException, CustomerException, LogHandlerException{
+		pr.processLog(logThree);
+		assertEquals(100, pr.getNumPizzaOrders());
+	}
+	
+	@Test
+	public void totalOrderProfit() throws PizzaException, CustomerException, LogHandlerException{
+		pr.processLog(logOne);
+		assertEquals(36.5, pr.getTotalProfit(), 0);
+	}
+	
+	@Test
+	public void testReset() throws PizzaException, CustomerException, LogHandlerException{
+		pr.processLog(logTwo);
+		pr.resetDetails();
+		assertEquals(0, pr.getNumPizzaOrders());
+		assertEquals(0, pr.getTotalProfit(), 0);
 	}
 }
