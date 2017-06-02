d873a2f81baabb9eb821e5d45c0ee341b400ea41
MrOswaldDoring <oswald.doring98@hotmail.com>
2017-06-02 15:34:17 +1000

    minor changes due to error

--- src/asgn2Customers/Customer.java
+++ src/asgn2Customers/Customer.java
@@ -8,7 +8,7 @@ import asgn2Exceptions.CustomerException;
  *  the abstract method getDeliveryDistance. A description of the class's
  * fields and their constraints is provided in Section 5.2 of the Assignment Specification.  
  * 
- * @author Oswald Doring n9451269
+ * @author Oswald Doring n9451269;
 */
 public abstract class Customer {
 	
